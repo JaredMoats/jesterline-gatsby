28aa1233c0aeed7392797f35817469ec2840f79e
Jared Moats <JaredMoats@outlook.com>
2019-05-16 13:45:06 -0400

    Fixed layout component bug

--- src/pages/how-it-works.js
+++ src/pages/how-it-works.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from "./../components/Layout";
+import Layout from "./../components/layout";
 import StandardLanding from "./../components/StandardLanding";
 import StandardPageView from "./../components/StandardPageView";
 import Prices from "./../components/Prices";
