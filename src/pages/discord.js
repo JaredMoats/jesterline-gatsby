b9c7c7d53770ab746a7d3a3a429cb0c94ca25ff2
Jared Moats <JaredMoats@outlook.com>
2019-06-12 14:43:50 -0400

    Added discord redirect

---
+++ src/pages/discord.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from "../components/layout";
+
+const discord = () => {
+    return (
+        <Layout>
+            {window.location = "https://discord.gg/r27uT6f"}
+        </Layout>
+    )
+}
+
+export default discord;
